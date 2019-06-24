587983490aeaafaedd8a4c5b903b59563d01591d
Ekaterina Timchenko <ekaterina.timchenko@nixsolutions.com>
2019-06-24 11:54:04 +0300

    Add changes in Footer

--- src/layout/components/Footer/Footer.jsx
+++ src/layout/components/Footer/Footer.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import './footer.scss';
 
 function Footer() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <footer className="footer">
-          <Container maxWidth="lg">
-            <Typography variant="h6" align="center" gutterBottom>
-              Footer
-            </Typography>
-          </Container>
-          <Container className="footerIcons">
-            <i className="material-icons">face</i>
-            <i className="material-icons">grade</i>
-            <i className="material-icons">language</i>
-          </Container>
-        </footer>
-      </Toolbar>
-    </AppBar>
+    <footer className="footer">
+      <Container className="footerContainer">
+        <Container maxWidth="lg">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+        </Container>
+        <Container className="footerIcons">
+          <i className="material-icons">face</i>
+          <i className="material-icons">grade</i>
+          <i className="material-icons">language</i>
+        </Container>
+      </Container>
+    </footer>
   );
 }
 
